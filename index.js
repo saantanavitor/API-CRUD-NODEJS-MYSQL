36046029901e0ae2609df470dbdaed90243616d2
saantanavitor <saantanavitor@gmail.com>
2023-04-19 09:54:21 -0300

    Atualização e removação de pet por ID

--- index.js
+++ index.js
@@ -99,12 +99,12 @@ app.post("/pets/", async (req, res) => {  // Adicionar novo pet
     }
 });
 
-app.get("/pets/", async (req, res) => {
+app.get("/pets/", async (req, res) => {   // Lista de pets
     const listaPets = await Pet.findAll();
     res.json(listaPets);
 });
 
-app.get("/pets/:id", async (req, res) => {
+app.get("/pets/:id", async (req, res) => {   // Buscar pet por ID
     const {id} = req.params;
 
     const pet = await Pet.findByPk(id);
@@ -117,6 +117,42 @@ app.get("/pets/:id", async (req, res) => {
     }
 
 });
+
+app.put("/pets/:id", async (req, res) => {   // Atualizar pet por ID
+    //dados que virao no corpo do json
+    const { nome, raca, porte, dtNasc } = req.body;
+    //checar a existencia do pet no sistema (select * from pets where id= req.params.id)
+    const pet = await Pet.findByPk(req.params.id);
+
+    try {
+        if(pet) {
+            //atualiza
+            //IMPORTANTE indicar qual pet atualizar!! COLOCAR A CONDIÇÃO where
+            // 1º argumento: dados novos, 2º argumento: where(condicao de busca: id)
+            await Pet.update({nome, raca, porte, dtNasc}, {where: {id: req.params.id}});
+            res.json({message: "Pet atualizado."});
+        } else {
+            //retorna 404, caso id invalido
+            res.status(404).json({message: "Pet não encontrado."})
+        }
+    } catch(err) {
+        //retorna 500, caso erro inesperado
+        console.log(err);
+        res.status(500).json({message: "Um erro aconteceu ao tentar conectar com o servidor."})
+    }
+});
+
+app.delete("/pets/:id", async (req,res) => { // Deletar pet por ID
+        // checar se o pet existe antes de apagar
+        const pet = await Pet.findByPk(req.params.id);
+    
+        if(pet) {       // se existe, pode apagar
+            await pet.destroy();
+            res.json({message: "Pet removido."})
+        } else {
+            res.status(404).json({message: "Pet nao encontrado."})
+        }
+    });
 
 app.listen(3000, () => {
     connection.sync({force: true});
